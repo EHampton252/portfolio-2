9ec2ae6c29d9658140fd3afc4ce0243d60d68e77
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-05 20:28:35 -0400

    adds nav bar

--- Portfolio-2/src/App.tsx
+++ Portfolio-2/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './Components/navHeader'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <h1>Home</h1>
       <h2>Hello!</h2>
       <h3>About Me</h3>
---
+++ Portfolio-2/src/Components/navHeader.tsx
@@ -0,0 +1,23 @@
+import "./navHeader.css"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                    <div className="header-text">
+                        <h2>Ethan Hampton</h2>
+                    </div>
+                <div className="header-links">
+                    <ul className="nav">
+                        <li><a href="./#home">Home</a></li>
+                        <li><a href="./#about">About</a></li>
+                        <li><a href="./#projects">Projects</a></li>
+                        <li><a href="./#contact">Contact Me</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
