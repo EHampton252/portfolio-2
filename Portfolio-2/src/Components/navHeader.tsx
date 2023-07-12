0f30081dbfb47df3249a1410f8c8b3c4188a34e9
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-11 21:04:52 -0400

    Finally decided on CSS classes over tailwind

--- Portfolio-2/src/Components/navHeader.tsx
+++ Portfolio-2/src/Components/navHeader.tsx
@@ -3,19 +3,14 @@ import "./navHeader.css"
 const Header = () => {
     return (
         <header className="header">
-            <div className="header-content">
-                    <div className="header-text">
-                        <h2>Ethan Hampton</h2>
-                    </div>
-                <div className="header-links">
-                    <ul className="nav">
+                <nav className="header-links">
+                    <ul className="navigation">
                         <li><a href="./#home">Home</a></li>
                         <li><a href="./#about">About</a></li>
                         <li><a href="./#projects">Projects</a></li>
                         <li><a href="./#contact">Contact</a></li>
                     </ul>
-                </div>
-            </div>
+                </nav>
         </header>
     )
 }
